b4f3c9c2adc725928301ba88bddf8d109f79afe0
KhalidMimouni <74767260+KhalidMimouni@users.noreply.github.com>
2022-01-19 19:09:20 +0100

    bugfixes for no host found

--- index.js
+++ index.js
@@ -7,31 +7,24 @@ const port = process.env.PORT || 3000
 var hosts = []
 
 io.on('connection', socket => {
-    
+
     socket.on('host-join', (roomId) => {
         console.log('host joined room')
-        
-        var host = {socketId: socket.id, roomId: roomId}
+
+        var host = { socketId: socket.id, roomId: roomId }
         hosts.push(host)
         socket.to(roomId).emit('host-id', socket.id)
         socket.on('disconnect', () => {
             console.log('host disconnected from room')
-            const indexOfHostInArray = hosts.indexOf(host)
-            hosts.splice(indexOfHostInArray, 1)
+            deleteHostFromArray(host)
+            socket.to(roomId).emit('no-host-found')
         })
     })
     socket.on('guest-join', (roomId) => {
         console.log('guest joined room')
         socket.join(roomId)
-        var host = hosts.find(host => host.roomId == roomId)
-        
-        if(host){
-            io.to(socket.id).emit('host-id', host.socketId)
-        }
-        else{
-            io.to(socket.id).emit('no-host-found')
-        }
-        
+        getHostSocketId(roomId)
+
     })
     socket.on('kickguests', (roomId) => {
         io.to(roomId).emit('leave', roomId)
@@ -43,7 +36,7 @@ io.on('connection', socket => {
     socket.on('message-to-host', (userId, message) => {
         console.log(userId)
         console.log(message)
-        
+
         io.to(userId).emit('message-from-guest', message)
     })
     socket.on('message-to-guest', (userId, message) => {
@@ -51,5 +44,20 @@ io.on('connection', socket => {
     })
 
 })
+
+function deleteHostFromArray(host) {
+    const indexOfHostInArray = hosts.indexOf(host)
+    hosts.splice(indexOfHostInArray, 1)
+}
+function getHostSocketId(roomId) {
+    const host = hosts.find(host => host.roomId == roomId)
+
+    if (host) {
+        io.to(socket.id).emit('host-id', host.socketId)
+    }
+    else {
+        io.to(socket.id).emit('no-host-found')
+    }
+}
 
 httpserver.listen(port, () => console.log(`signalling server is listening on port ${port}`))
